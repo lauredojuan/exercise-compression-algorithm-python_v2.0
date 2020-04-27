4d9a91df6d2f0c68f63818e3ee2c2c475af542e5
Juan L <lauredojuan@gmail.com>
2020-04-27 19:47:33 +0000

    load csv added

--- compress.py
+++ compress.py
@@ -1,20 +1,37 @@
 # !/usr/bin/python
 # coding=utf-8
 import re
+import csv
 
-symbols = {
-    "implementation": "🤯",
-    "practicality": '🤩',
-    "better": '😅',
-    "than": '😘',
-    "Although": "🥺"
-}
+symbols = {}
+# symbols = {
+#     "implementation": "🤯",
+#     "practicality": '🤩',
+#     "better": '😅',
+#     "than": '😘',
+#     "Although": "🥺"
+# }
+
+def load_symbols():
+  global symbols
+  try:
+    with open("symbols.csv", newline='') as myFile:
+      reader = csv.reader(myFile)
+      for rows in reader:
+        symbols = rows
+  except:
+    open("symbols.csv", "w+")
+    load_symbols()
 
 def compress(content):
+    load_symbols()
     compressed_content = content
+
     for x in symbols:
         compressed_local = compressed_content.replace(x, symbols[x])
         compressed_content = compressed_local
 
     return compressed_content
+
+
     
